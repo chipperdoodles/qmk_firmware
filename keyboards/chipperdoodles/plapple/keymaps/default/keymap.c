b4435f8c3cef2b080c32c43b874c06d975aee104
chipperdoodles <chipperdoodles@protonmail.com>
2020-05-09 16:24:23 -0600

    Changes to be committed: 	modified:   keyboards/chipperdoodles/plapple/keymaps/default/keymap.c

--- keyboards/chipperdoodles/plapple/keymaps/default/keymap.c
+++ keyboards/chipperdoodles/plapple/keymaps/default/keymap.c
@@ -24,21 +24,29 @@
 // entirely and just use numbers.
 #define _BL 0
 #define _FL 1
+#define _ML 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
-    KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   KC_BSPC,     KC_NLCK, KC_PSLS,  KC_PAST, KC_PMNS, \
-    KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,  KC_BSLS,     KC_P7,   KC_P8,  KC_P9, KC_PPLS, \
-    KC_CAPS,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT,   KC_ENT,             KC_P4,   KC_P5,  KC_P6, KC_PEQL, \
+    KC_GESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   KC_BSPC,     KC_NLCK, KC_PEQL,  KC_SLS, KC_PAST, \
+    KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,  KC_BSLS,     KC_P7,   KC_P8,  KC_P9, KC_PMNS, \
+    KC_CAPS,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT,   KC_ENT,             KC_P4,   KC_P5,  KC_P6, KC_PPLS, \
     KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   KC_RSFT,                  KC_P1,   KC_P2,  KC_P3, KC_PENT, \
     KC_LCTL,  KC_LGUI,  KC_LALT,                 KC_SPC,             KC_RALT,   KC_RGUI,   MO(_FL),   KC_RCTL,                  KC_P0,   KC_P0,  KC_PDOT, KC_PENT  \
     ),
     [_FL] = LAYOUT(
-    RESET,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   KC_BSPC,      KC_NLCK, KC_PSLS,  KC_MYCM, KC_CALC, \
-    OUT_USB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_P7,   KC_VOLU,  KC_MUTE, KC_PPLS, \
-    OUT_BT,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT,   KC_ENT,              KC_MPRV, KC_MPLY,  KC_MNXT, KC_PEQL, \
-    KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   KC_RSFT,                  KC_P1,   KC_VOLD,  KC_P3,   KC_PENT, \
-    KC_LCTL,  KC_LGUI,  KC_LALT,                 KC_SPC,             KC_RALT,   KC_RGUI,   MO(_FL),   KC_RCTL,                  KC_P0,   KC_P0,    KC_PDOT, KC_PCMM  \
+    RESET,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   KC_BSPC,      KC_NLCK, KC_PSCR,  KC_MYCM, KC_CALC, \
+    OUT_USB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_BRIU,   KC_VOLU,  KC_MUTE, KC_MFFD, \
+    OUT_BT,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT,   KC_ENT,              KC_MPRV, KC_MPLY,  KC_MNXT, KC_MRWD, \
+    KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   KC_RSFT,                  KC_BRID,   KC_VOLD,  KC_P3,   KC_PENT, \
+    KC_LCTL,  KC_LGUI,  KC_LALT,                 TO(_ML),             KC_RALT,   KC_RGUI,  KC_TRNS,   KC_RCTL,                  KC_CGTOGG,   KC_P0,    KC_PDOT, KC_PENT  \
+    ),
+    [_ML] = LAYOUT(
+    KC_GESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   KC_BSPC,      KC_NLCK, KC_PSLS,  KC_MYCM, KC_CALC, \
+    KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_BTN1, KC_MS_U,  KC_BTN2, KC_WH_U, \
+    KC_CAPS,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT,   KC_ENT,              KC_MS_L, KC_BTN3,  KC_MS_R, KC_WH_D, \
+    KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   KC_RSFT,                  KC_BTN4, KC_MS_D,  KC_BTN5, KC_PENT, \
+    KC_LCTL,  KC_LGUI,  KC_LALT,                 TO(_BL),             KC_RALT,   KC_RGUI,   KC_TRNS,   KC_RCTL,                  KC_WH_L, KC_WH_R,  KC_PDOT, KC_PENT  \
     ),
 };
 
